35c064b49b70258ea142fb768d5ae05a4bf75bf0
zpaaa <1194972448@qq.com>
2018-04-25 00:10:40 +0800

    1.0

---
+++ js/index.js
@@ -0,0 +1,8 @@
+<<<<<<< HEAD
+$(".demo-1").click(function(){
+	console.log(1)
+=======
+$("#welcome").click(function(){
+	$(this).fadeOut(1000).siblings().css({"display":"block"});
+>>>>>>> v1.0
+})
