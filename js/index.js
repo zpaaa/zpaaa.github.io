b740170063f6617b1982fe5ca07ea15628729b95
zpaaa <37869987+zpaaa@users.noreply.github.com>
2018-04-25 15:38:18 +0800

    Delete index.js

--- js/index.js
+++
@@ -1,8 +0,0 @@
-<<<<<<< HEAD
-$(".demo-1").click(function(){
-	console.log(1)
-=======
-$("#welcome").click(function(){
-	$(this).fadeOut(1000).siblings().css({"display":"block"});
->>>>>>> v1.0
-})
